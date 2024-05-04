172f1be38a563578f7ae0a6778391e17c07760e9
Damien4822 <114857862+Damien4822@users.noreply.github.com>
2024-05-04 19:37:27 +0700

    Adding Entities

---
+++ Timesheet/src/main/java/project/timesheet/Entity/LichLamViec.java
@@ -0,0 +1,35 @@
+package DuAnChamCong.backend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Time;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "LichLamViec")
+public class LichLamViec {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @Nullable
+    private VanPhong vpCongTac;
+    @ManyToOne
+    private TrangThaiLamViec trangThai;
+    @ManyToOne
+    private NhanVien nhanVien;
+    private Date ngayLam;
+    private Time gioBatDau;
+    private Time gioKetThuc;
+    private String tenCongViec;
+
+}
---
+++ Timesheet/src/main/java/project/timesheet/Entity/User.java
@@ -0,0 +1,30 @@
+package DuAnChamCong.backend.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    // remaining getters and setters are not shown for brevity
+}
